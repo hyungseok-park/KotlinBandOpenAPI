5c3be2ee3eef4caa95dc9b48107ee4b6316a204a
hyungseok.park <hyungseok.park@navercorp.com>
2023-09-04 17:44:19 +0900

    add functions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.21"
     application
 }
 
-group = "org.example"
+group = "KotlinBandOpenAPI"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("com.typesafe:config:1.4.1")
     implementation("io.ktor:ktor-http:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(11)
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
+
 
 application {
     mainClass.set("MainKt")
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,18 +4,69 @@ import java.net.HttpURLConnection
 import java.net.URL
 import io.ktor.http.*
 import com.typesafe.config.ConfigFactory
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+
+val config = ConfigFactory.load("app.properties")
+val accessToken = config.getString("band.token")
+val apiHost = config.getString("api.host")
 
 fun main() {
+    queryMyInfo()
+    queryMyBands()
+}
 
-    val config = ConfigFactory.load("app.properties")
-    val accessToken = config.getString("band.token")
-    val apiHost = config.getString("api.host")
-    val apiVersion = config.getString("api.version")
+/**
+    query my information
+    https://developers.band.us/develop/guide/api/get_user_information
+ */
+fun queryMyInfo() {
+    val apiVersion = "v2"
+    val jsonResult = httpRequest(apiVersion, "profile") ?: return
+    val resultData = jsonResult.jsonObject["result_data"] ?: return
+    val myName = resultData.jsonObject["name"]
+    val profileImg = resultData.jsonObject["profile_image_url"]
+    println("myName: $myName, profileImg: $profileImg")
+}
 
+/**
+query my BANDs
+https://developers.band.us/develop/guide/api/get_user_information
+ */
+fun queryMyBands() {
+    val apiVersion = "v2.1"
+    val jsonResult = httpRequest(apiVersion, "bands") ?: return
+    val resultData = jsonResult.jsonObject["result_data"] ?: return
+    val bands = resultData.jsonObject["bands"] ?: return
+    bands.jsonArray.map {
+        println("band: ${it.jsonObject["name"]}, band key: ${it.jsonObject["band_key"]}")
+    }
+}
+
+/**
+query posts in a BAND
+https://developers.band.us/develop/guide/api/get_user_information
+ */
+fun queryPosts() {
+    val apiVersion = "v2"
+    val jsonResult = httpRequest(apiVersion, "band/posts", mapOf<String, String>()) ?: return
+    val resultData = jsonResult.jsonObject["result_data"] ?: return
+    val bands = resultData.jsonObject["bands"] ?: return
+    bands.jsonArray.map {
+        println("band: ${it.jsonObject["name"]}")
+    }
+}
+
+
+
+
+private fun httpRequest(apiVersion: String, api: String): JsonElement? {
     val url = URLBuilder().apply {
         protocol = URLProtocol.HTTPS
         host = apiHost
-        path(apiVersion, "bands")
+        path(apiVersion, api)
         parameters.append("access_token", accessToken)
     }.buildString()
     val connection = URL(url).openConnection() as HttpURLConnection
@@ -27,12 +78,12 @@ fun main() {
         if (responseCode == HttpURLConnection.HTTP_OK) {
             val inputStream = connection.inputStream
             val reader = BufferedReader(InputStreamReader(inputStream))
-            val response = reader.readText()
+            val jsonElement = Json.parseToJsonElement(reader.readText())
             reader.close()
-
-            println(response)
+            return jsonElement
         } else {
             println("Failed to get a valid response")
+            return null
         }
     } finally {
         connection.disconnect()
